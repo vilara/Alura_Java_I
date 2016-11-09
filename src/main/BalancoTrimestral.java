7d08edb7be1298e419efce9e1ef1842e3b7b97d0
Marcelo Vilara <marcelovilara@gmail.com>
2016-11-09 20:06:04 -0200

    exercicio 1

---
+++ src/main/BalancoTrimestral.java
@@ -0,0 +1,11 @@
+package main;
+
+public class BalancoTrimestral {
+public static void main(String[] args) {
+	int gastoJaneiro = 1500;
+	int gastoFevereiro = 23000;
+	int gastoMarco = 17000;
+	int gastoTrimestre = gastoJaneiro + gastoFevereiro + gastoMarco;
+	System.out.println(gastoTrimestre);
+}
+}
