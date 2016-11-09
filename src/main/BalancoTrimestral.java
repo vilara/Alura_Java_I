21848fad7e2315756ef2a62c47162bcb6e19ab9c
Marcelo Vilara <marcelovilara@gmail.com>
2016-11-09 21:19:03 -0200

    exercicio 2

--- src/main/BalancoTrimestral.java
+++ src/main/BalancoTrimestral.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
 	int gastoMarco = 17000;
 	int gastoTrimestre = gastoJaneiro + gastoFevereiro + gastoMarco;
 	System.out.println(gastoTrimestre);
+	double mediaMensal = gastoTrimestre/3;
+	System.out.println("A média mensal é = "+mediaMensal);
 }
 }
