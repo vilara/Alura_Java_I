198e3ff7cd05574e407dcfc4f38ee21dba6e48fd
Marcelo Vilara <marcelovilara@gmail.com>
2016-11-09 21:23:08 -0200

    exercicio 3

---
+++ src/main/NumeroSequencial.java
@@ -0,0 +1,11 @@
+package main;
+
+public class NumeroSequencial {
+public static void main(String[] args) {
+	int numero = 150;
+	while (numero < 300) {
+		System.out.println(numero);
+		numero++;
+	}
+}
+}
