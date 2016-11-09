c06e0522e37059ab9c5805b5720a11233031b5e2
Marcelo Vilara <marcelovilara@gmail.com>
2016-11-09 18:42:14 -0200

    inicio do projeto

---
+++ src/main/MeuPrimeiroPrograma.java
@@ -0,0 +1,8 @@
+package main;
+
+public class MeuPrimeiroPrograma {
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa!");
+		System.out.println("segunda linha do programa!");
+	}
+}
