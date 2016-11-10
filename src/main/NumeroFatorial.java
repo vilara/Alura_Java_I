63ba1d196ba3b80ec7b0c3f1acfc0be6b9d4639b
Marcelo Vilara <marcelovilara@gmail.com>
2016-11-09 23:44:14 -0200

    cap2 ex1

--- src/main/NumeroFatorial.java
+++ src/main/NumeroFatorial.java
@@ -4,8 +4,8 @@ public class NumeroFatorial {
 public static void main(String[] args) {
 	int numero = 1;
 	
-	while (numero <=10) {
-		int fatorial  =  1;
+	while (numero <=30) {
+		long fatorial  =  1;
 		for (int i = numero; i > 0; i--) {
 			
 			fatorial = fatorial*i;
